2963fda43c4c9fb406701f64cfc7d55bfb7088af
Konstanitn <90149772+kostiashkaaa@users.noreply.github.com>
2026-03-06 22:36:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-# TOKEN ="7975904976:AAESTV8fzF1m5noQs-gkjAQbyD9AxvFhHJU"
-# тестовый токен для бота https://t.me/test_medicalllll_bot
-TOKEN = "8714773904:AAFtu6yyMC7Ne1Ogxe2Xrd_fCrKkgPChYVc"
+TOKEN = "7975904976:AAESTV8fzF1m5noQs-gkjAQbyD9AxvFhHJU" #ОСНОВНОЙ ТОКЕН ДЛЯ https://t.me/idukvrachubot
+#TOKEN = "8714773904:AAFtu6yyMC7Ne1Ogxe2Xrd_fCrKkgPChYVc" # Тестовый токен для бота https://t.me/test_medicalllll_bot
