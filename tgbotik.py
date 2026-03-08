ff48d6914c6a5fd3fbacbbf492ce6df753b25398
emel1ch <andruha.emeltv@gmail.com>
2026-03-08 22:56:41 +0300

    Внесены фиксы в работу календаря и добавлена полноценная поддержка

--- tgbotik.py
+++ tgbotik.py
@@ -13,6 +13,9 @@ from config import TOKEN, GROUP_ID
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+git add .
+git commit -m "Описание того, что было сделано"
+git push
 
 # --- Состояния ---
 class UserState(StatesGroup):
