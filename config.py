5aa6b8dd2237a1b32ea0b58355a15b846bc16469
emel1ch <andruha.emeltv@gmail.com>
2026-03-08 21:58:35 +0300

    Сохранил рабочий код бота перед слиянием

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 TOKEN = "7975904976:AAESTV8fzF1m5noQs-gkjAQbyD9AxvFhHJU" #ОСНОВНОЙ ТОКЕН ДЛЯ https://t.me/idukvrachubot
+GROUP_ID = -1003842744166 #ID беседы для поддержки
 #TOKEN = "8714773904:AAFtu6yyMC7Ne1Ogxe2Xrd_fCrKkgPChYVc" # Тестовый токен для бота https://t.me/test_medicalllll_bot
